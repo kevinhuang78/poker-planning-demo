53bf0c890652a460fc65dcc7ce7df8c1e4d2e8f6
kevinhuang78 <kevin_huang_iim@yahoo.com>
2024-09-20 12:33:29 +0200

    feat: add average

--- client/src/components/users/users-list.tsx
+++ client/src/components/users/users-list.tsx
@@ -2,6 +2,25 @@ import type {CardEvent, User as UserType} from "../../App";
 import User from "./user";
 import './users.css';
 
+const CARD_VALUES = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+const getCardValuesStats = (allUsers: UsersListProps['allUsers']) => {
+  // @ts-ignore
+  const allCardValues: number[] = allUsers.filter((user) => typeof user.cardValue === 'number').map((user) => user.cardValue);
+  const numberOfUserThatVoted = allCardValues.length;
+  const sumOfAll = allCardValues.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0,
+  );
+  const average = sumOfAll / numberOfUserThatVoted;
+  const nearestCard = CARD_VALUES.reduce((prev, curr) => Math.abs(curr - average) < Math.abs(prev - average) ? curr : prev);
+
+  return {
+    average,
+    nearestCard,
+  }
+}
+
 export type UsersListProps = {
   selfSelectedCard?: CardEvent['value'];
   selfClientId: string;
@@ -9,12 +28,24 @@ export type UsersListProps = {
   shouldCardsBeFlipped: boolean;
 };
 
-const UsersList = ({ selfClientId, allUsers, shouldCardsBeFlipped }: UsersListProps) => (
-  <div className='users'>
-    {allUsers.map((user) => (
-      <User key={user.clientId} selfClientId={selfClientId} user={user} shouldCardsBeFlipped={shouldCardsBeFlipped} />
-    ))}
-  </div>
-);
+const UsersList = ({ selfClientId, allUsers, shouldCardsBeFlipped }: UsersListProps) => {
+  const { average, nearestCard } = getCardValuesStats(allUsers);
+
+  return (
+    <>
+      {shouldCardsBeFlipped && (
+        <div className='users-stats'>
+          <p>{`The average of the card values is: ${average}`}</p>
+          <p>{`The nearest card is: ${nearestCard}`}</p>
+        </div>
+      )}
+      <div className='users'>
+        {allUsers.map((user) => (
+          <User key={user.clientId} selfClientId={selfClientId} user={user} shouldCardsBeFlipped={shouldCardsBeFlipped} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default UsersList;
