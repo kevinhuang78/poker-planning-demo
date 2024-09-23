dbb724422c62e89735f0a06adf02de3db3fa598b
kevinhuang78 <kevin_huang_iim@yahoo.com>
2024-09-23 17:54:23 +0200

    misc: show card with 0 value

--- client/src/components/users/user.tsx
+++ client/src/components/users/user.tsx
@@ -1,14 +1,17 @@
-import type {User as UserType} from "../../App";
+import type {CardValue, User as UserType} from "../../App";
 import type {UsersListProps} from "./users-list";
 import Card from "../card/card";
+
+const shouldShowCardValue = (cardValue?: CardValue) => typeof cardValue === 'number' || !!cardValue;
 
 type GetCardValueParams = Pick<UserProps, 'user' | 'shouldCardsBeFlipped'> & { isSelf: boolean; };
 
 const getCardValue = ({ user, shouldCardsBeFlipped, isSelf }: GetCardValueParams) => {
+  const isCardValueFilled = shouldShowCardValue(user.cardValue);
   if (isSelf) return user.cardValue;
-  if (shouldCardsBeFlipped) return user.cardValue || undefined;
+  if (shouldCardsBeFlipped) return isCardValueFilled ? user.cardValue : undefined;
 
-  return user.cardValue ? '...' : undefined;
+  return isCardValueFilled ? '...' : undefined;
 }
 
 type UserProps = Pick<UsersListProps, 'selfClientId' | 'shouldCardsBeFlipped'> & {
@@ -20,14 +23,16 @@ const User = ({ selfClientId, user, shouldCardsBeFlipped }: UserProps) => {
   const isSelf = clientId === selfClientId;
   const cardValue = getCardValue({ user, shouldCardsBeFlipped, isSelf });
   const usernameToShow = username || defaultUsername;
+  const isCardValueFilled = shouldShowCardValue(user.cardValue);
 
   return (
     <div className='user'>
       <p className={`user-username ${isSelf ? 'user-username--isSelf' : ''}`}>
         {isSelf ? `You: ${usernameToShow}` : usernameToShow}
       </p>
-      {cardValue ? (
+      {isCardValueFilled ? (
         <div className='user-card'>
+          {/* @ts-expect-error TS does not understand cardValue is always a number or a string here */}
           <Card value={cardValue} onClick={() => {}} />
         </div>
       ) : <p className='user-no-card-selected'>No card selected yet</p>}
